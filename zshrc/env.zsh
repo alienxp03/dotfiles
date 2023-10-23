610ca701dd34773081e6460866b4b3c8dd42fa78
Azuan <alienxp03@gmail.com>
2023-10-23 10:03:39 +0800

    Add diff-so-fancy, tmux shortcuts via Alacritty

--- zshrc/env.zsh
+++ zshrc/env.zsh
@@ -1,13 +1,5 @@
-export WORKSPACE="$HOME/Workspace"
-export ZSH="$HOME/.oh-my-zsh"
-export ZSH_THEME="powerlevel10k/powerlevel10k"
-export EDITOR='nvim'
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
-export RUBY_CONFIGURE_OPTSx="--with-openssl-dir=/opt/openssl-1.1.1s/"
-export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
-export LIBGL_ALWAYS_SOFTWARE=1
-export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
 export PATH="$WORKSPACE/bin/apache-maven-3.8.6/bin:$PATH"
 export PATH="$WORKSPACE/bin/AssetRipperConsole_linux64:$PATH"
 export PATH="$WORKSPACE/bin/:$PATH"
@@ -15,6 +7,16 @@ export PATH="/usr/local/bin/:$PATH"
 export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
 export PATH="$HOME/.tmux/plugins/tmuxifier/bin:$PATH"
 export PATH="$HOME/.tmux/plugins/t-smart-tmux-session-manager/bin:$PATH"
+export PATH="$WORKSPACE/GitHub/diff-so-fancy:$PATH"
+
+export WORKSPACE="$HOME/Workspace"
+export ZSH="$HOME/.oh-my-zsh"
+export ZSH_THEME="powerlevel10k/powerlevel10k"
+export EDITOR='nvim'
+export RUBY_CONFIGURE_OPTSx="--with-openssl-dir=/opt/openssl-1.1.1s/"
+export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
+export LIBGL_ALWAYS_SOFTWARE=1
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
 export TMUXIFIER_LAYOUT_PATH="$HOME/.tmux-layouts"
 export DOCKER_BUILDKIT=1
 export DOTFILES=~/.dotfiles
