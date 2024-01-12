0c76f824254bb2a74338d78f340fa57afd51b399
azuan <alienxp03@gmail.com>
2024-01-12 23:18:21 +0800

    Update ruby_ls

--- nvim/lua/user/plugins/gitsigns.lua
+++ nvim/lua/user/plugins/gitsigns.lua
@@ -8,14 +8,6 @@ return {
       "CursorColumn",
       "Whitespace",
     }
-    require("ibl").setup({
-      indent = { highlight = highlight, char = "┊" },
-      whitespace = {
-        highlight = highlight,
-        remove_blankline_trail = false,
-      },
-      scope = { enabled = false },
-    })
     require("gitsigns").setup()
   end,
 }
