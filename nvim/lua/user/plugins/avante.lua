c9bd9ea5115b478cd6dba9160e21dda11e5e312f
alienxp03 <alienxp03@gmail.com>
2025-03-09 23:05:24 +0800

    Avante

--- nvim/lua/user/plugins/avante.lua
+++ nvim/lua/user/plugins/avante.lua
@@ -1,17 +1,24 @@
 return {
-  -- "yetone/avante.nvim",
-  -- event = "VeryLazy",
-  -- lazy = true,
-  -- build = "make BUILD_FROM_SOURCE=true",
-  -- opts = {
-  --   provider = "gemini",
-  --   gemini = {
-  --     endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-  --     -- model = "gemini-1.5-flash-latest",
-  --     model = "gemini-2.0-flash-exp",
-  --     timeout = 30000, -- Timeout in milliseconds
-  --     temperature = 0,
-  --     max_tokens = 4096,
-  --   },
-  -- },
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = "*",
+  lazy = true,
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    provider = "gemini",
+    gemini = {
+      endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+      -- model = "gemini-1.5-flash-latest",
+      model = "gemini-2.0-flash-exp",
+      timeout = 30000, -- Timeout in milliseconds
+      temperature = 0,
+      max_tokens = 4096,
+    },
+  },
 }
