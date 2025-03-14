8b08c5a5888b75fca3d8a77e32f9648d9fbe2d4c
alienxp03 <alienxp03@gmail.com>
2025-03-14 15:47:31 +0800

    Alacritty use toml

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 ln -nfs ~/.dotfiles/nvim ~/.config/nvim
-ln -nfs ~/.dotfiles/.alacritty.yml ~/.alacritty.yml
+ln -nfs ~/.dotfiles/.alacritty.toml ~/.alacritty.toml
 ln -nfs ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -nfs ~/.dotfiles/.zshrc ~/.zshrc
 ln -nfs ~/.dotfiles/.tool-versions ~/.tool-versions
