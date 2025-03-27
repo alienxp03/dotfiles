e923c798e2a8af2b69c08a884893c28c86dec2e6
alienxp03 <alienxp03@gmail.com>
2025-03-27 13:31:48 +0800

    Update lualine

--- nvim/lua/user/plugins/lualine.lua
+++ nvim/lua/user/plugins/lualine.lua
@@ -2,6 +2,39 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("lualine").setup()
+    require("lualine").setup({
+      extensions = {
+        "nvim-tree",
+      },
+      options = {
+        theme = "gruvbox",
+      },
+      sections = {
+        lualine_b = { "diff", "diagnostics" },
+        lualine_c = {
+          {
+            "filename",
+            path = 1,
+          },
+          {
+            "filetype",
+          },
+        },
+        lualine_x = {
+          {
+            "lsp_status",
+            icon = "",
+            symbols = {
+              -- Standard unicode symbols to cycle through for LSP progress:
+              spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+              done = "✓",
+              separator = " ",
+            },
+            ignore_lsp = {},
+          },
+        },
+        lualine_y = {},
+      },
+    })
   end,
 }
