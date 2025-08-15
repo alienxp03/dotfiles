69501ba74116a50335157103f498ad7481e085ef
alienxp03 <alienxp03@gmail.com>
2025-08-15 22:56:43 +0800

    Update gitsigns

--- nvim/lua/user/plugins/gitsigns.lua
+++ nvim/lua/user/plugins/gitsigns.lua
@@ -2,10 +2,19 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    local highlight = {
-      "CursorColumn",
-      "Whitespace",
-    }
     require("gitsigns").setup()
+
+    vim.keymap.set(
+      "n",
+      "<leader>ge",
+      ":Gitsigns toggle_current_line_blame<cr>",
+      { noremap = true, silent = true, desc = "Toggle blame" }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>gh",
+      ":Gitsigns preview_hunk_inline<cr>",
+      { noremap = true, silent = true, desc = "Preview hunk" }
+    )
   end,
 }
