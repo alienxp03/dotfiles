d9e16ed44e31d935c0f103673e36075152e96113
alienxp03 <alienxp03@gmail.com>
2026-06-27 19:42:24 +0100

    Fix markdown treesitter on Neovim 0.12

--- config/nvim/lua/user/plugins/nvim-treesitter.lua
+++ config/nvim/lua/user/plugins/nvim-treesitter.lua
@@ -9,73 +9,116 @@ return {
     "windwp/nvim-ts-autotag",
   },
   config = function()
-    -- v1.x: install parsers explicitly
-    local parsers = {
-      "lua",
-      "ruby",
-      "html",
-      "javascript",
-      "json",
-      "bash",
-      "css",
-      "go",
-      "java",
-      "query",
-      "scss",
-      "sql",
-      "typescript",
-      "tsx",
-      "vim",
-      "toml",
-      "yaml",
-    }
-    local config = require("nvim-treesitter.config")
-    local install = require("nvim-treesitter.install")
-    local installed = config.get_installed()
-    for _, lang in ipairs(parsers) do
-      if not vim.list_contains(installed, lang) then
-        install.install(lang)
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "ruby",
+        "html",
+        "javascript",
+        "json",
+        "bash",
+        "css",
+        "go",
+        "java",
+        "query",
+        "scss",
+        "sql",
+        "typescript",
+        "tsx",
+        "vim",
+        "toml",
+        "yaml",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      autotag = {
+        enable = true,
+        filetypes = { "html", "slim", "xml" },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
+        },
+      },
+    })
+
+    -- nvim-treesitter's markdown injection directive still receives the old
+    -- single-node capture shape, but Neovim 0.12 passes capture lists. Without
+    -- this shim, opening markdown files can spam `attempt to call method
+    -- 'range' (a nil value)` from vim.treesitter.get_node_text().
+    if vim.fn.has("nvim-0.12") == 1 then
+      local query = vim.treesitter.query
+      local aliases = {
+        ex = "elixir",
+        pl = "perl",
+        sh = "bash",
+        uxn = "uxntal",
+        ts = "typescript",
+      }
+
+      local function first_node(capture)
+        if type(capture) == "table" then
+          return capture[1]
+        end
+        return capture
       end
+
+      query.add_directive("set-lang-from-info-string!", function(match, _, bufnr, pred, metadata)
+        local node = first_node(match[pred[2]])
+        if not node or type(node.range) ~= "function" then
+          return
+        end
+
+        local alias = vim.treesitter.get_node_text(node, bufnr):lower()
+        metadata["injection.language"] = vim.filetype.match({ filename = "a." .. alias }) or aliases[alias] or alias
+      end, { force = true, all = false })
     end
 
-    -- v1.x: highlight and indent via autocmd
-    vim.api.nvim_create_autocmd("FileType", {
-      callback = function(args)
-        if pcall(vim.treesitter.start, args.buf) then
-          vim.bo[args.buf].indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
-        end
-      end,
-    })
-
-    -- autotag
     require("nvim-ts-autotag").setup({
-      filetypes = { "html", "slim", "xml" },
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = false,
+      },
     })
-
-    -- textobjects
-    require("nvim-treesitter-textobjects").setup()
-
-    local select = require("nvim-treesitter-textobjects.select")
-    local move = require("nvim-treesitter-textobjects.move")
-    local swap = require("nvim-treesitter-textobjects.swap")
-
-    vim.keymap.set({ "x", "o" }, "aa", function() select.select_textobject("@parameter.outer", "textobjects") end)
-    vim.keymap.set({ "x", "o" }, "ia", function() select.select_textobject("@parameter.inner", "textobjects") end)
-    vim.keymap.set({ "x", "o" }, "af", function() select.select_textobject("@function.outer", "textobjects") end)
-    vim.keymap.set({ "x", "o" }, "if", function() select.select_textobject("@function.inner", "textobjects") end)
-    vim.keymap.set({ "x", "o" }, "ac", function() select.select_textobject("@class.outer", "textobjects") end)
-    vim.keymap.set({ "x", "o" }, "ic", function() select.select_textobject("@class.inner", "textobjects") end)
-
-    vim.keymap.set({ "n", "x", "o" }, "]m", function() move.goto_next_start("@function.outer", "textobjects") end)
-    vim.keymap.set({ "n", "x", "o" }, "]]", function() move.goto_next_start("@class.outer", "textobjects") end)
-    vim.keymap.set({ "n", "x", "o" }, "]M", function() move.goto_next_end("@function.outer", "textobjects") end)
-    vim.keymap.set({ "n", "x", "o" }, "][", function() move.goto_next_end("@class.outer", "textobjects") end)
-    vim.keymap.set({ "n", "x", "o" }, "[m", function() move.goto_previous_start("@function.outer", "textobjects") end)
-    vim.keymap.set({ "n", "x", "o" }, "[[", function() move.goto_previous_start("@class.outer", "textobjects") end)
-    vim.keymap.set({ "n", "x", "o" }, "[M", function() move.goto_previous_end("@function.outer", "textobjects") end)
-    vim.keymap.set({ "n", "x", "o" }, "[]", function() move.goto_previous_end("@class.outer", "textobjects") end)
-
-    vim.keymap.set("n", "<leader>a", function() swap.swap_next("@parameter.inner") end)
-    vim.keymap.set("n", "<leader>A", function() swap.swap_previous("@parameter.inner") end)
   end,
 }
