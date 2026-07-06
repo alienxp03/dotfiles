85e0c655629b8529efe861ec260d29335e4de9c6
alienxp03 <alienxp03@gmail.com>
2026-07-06 15:44:58 +0100

    Update nvim-treesitter

--- config/nvim/lua/user/plugins/nvim-treesitter.lua
+++ config/nvim/lua/user/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 -- Highlight, edit, and navigate code
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
   dependencies = {
