ddad15bc99690c9df383989e9cfab9d7e0babf2f
alienxp03 <alienxp03@gmail.com>
2026-07-24 16:38:15 +0100

    Update ufo

--- config/nvim/lua/user/plugins/nvim-ufo.lua
+++ config/nvim/lua/user/plugins/nvim-ufo.lua
@@ -10,15 +10,8 @@ return {
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
 
-    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-    vim.keymap.set("n", "<leader>zr", require("ufo").openAllFolds, { desc = "Open all folds" })
-    vim.keymap.set("n", "<leader>zm", require("ufo").closeAllFolds, { desc = "Close all folds" })
-    vim.keymap.set("n", "<leader>zk", function()
-      local winid = require("ufo").peekFoldedLinesUnderCursor()
-      if not winid then
-        vim.lsp.buf.hover()
-      end
-    end, { desc = "Peek folded lines" })
+    vim.keymap.set("n", "<leader>la", require("ufo").closeAllFolds, { desc = "Close all folds" })
+    vim.keymap.set("n", "<leader>lr", require("ufo").openAllFolds, { desc = "Open all folds" })
 
     require("ufo").setup({
       provider_selector = function(bufnr, filetype, buftype)
